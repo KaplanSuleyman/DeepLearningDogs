aa9111a34ed5f660801332d77f7e610d446a7f76
sulo kaplan <kaplansuleyman11@gmail.com>
2021-03-12 17:12:10 +0100

    Projet

---
+++ README.md
@@ -0,0 +1,24 @@
+# Projet DeepLearning Dogs Classification
+
+## Description
+
+Ce projet a pour but de classifier une image de chien en fonction  de sa race. Pour atteindre cet objectif, nous utilisons un réseau neuronal (VGG16).
+
+## Installation
+
+* Veuillez récupérer le projet à l'aide de cette commande :
+  * git clone https://github.com/KaplanSuleyman/DeepLearningDogs
+* Ensuite, rendez-vous dans le dossier du projet :
+  * cd DeepLearningDogs
+* Installation des packages :
+  * pip install -r requirements.txt
+* Puis, lancer cette commande pour accéder au streamlit :
+  * streamlit run app.py
+
+
+* PS : Si vous voulez accéder au Notebook
+  * Tapez cette commande :
+    * jupyter notebook
+  * Il faut que vous télécharger le dataset de Stanford pour ensuite le dézipper à la racine du projet
+    * http://vision.stanford.edu/aditya86/ImageNetDogs/
+  * Vous pouvez maintenant accéder au notebook
---
+++ app.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import streamlit as st
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import load_model
+import cv2
+import tempfile
+import numpy as np
+from PIL import Image
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.applications.vgg16 import decode_predictions, preprocess_input
+from  tensorflow.keras.preprocessing.image import load_img, img_to_array
+import os
+
+def read_img(img): 
+    image = cv2.imread(img)
+    image = cv2.resize(image, (224,224))
+
+    return image
+
+st.title('Prédiction Chien')
+
+uploaded_file = st.file_uploader("Upload Image")
+
+if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+
+    st.image(opencv_image, channels="BGR")
+
+    dog_classes = os.listdir('./Images/')
+
+    X = []
+    y = []
+
+    X.append(opencv_image)
+    for breed in dog_classes:
+        y.append(breed)
+
+    encoder = LabelBinarizer()
+
+    X = np.array(X)
+    y = encoder.fit_transform(np.array(y))
+
+    if(st.button('Load model')):
+
+        model = load_model("./model.h5")
+
+        predictions = model.predict(X)
+
+        label_predictions = encoder.inverse_transform(predictions)
+
+        st.write(label_predictions)
